3f7eeeb51fa7fc7436f39e510465fba178a0c31d
hhasan1 <handy.hasan@yahoo.com>
2024-02-12 14:41:20 +1100

    test

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
+// react-app.js
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import singleSpaReact from 'single-spa-react';
+import App from './App'; // Your React App Component
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const reactLifecycles = singleSpaReact({
+  React,
+  ReactDOM,
+  rootComponent: App,
+  domElementGetter: () => document.getElementById('react-app'),
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export const bootstrap = [reactLifecycles.bootstrap];
+export const mount = [reactLifecycles.mount];
+export const unmount = [reactLifecycles.unmount];
